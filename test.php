88c3b7a9fcf4ad1027efb2c92c66923fe5b057df
Mehedihasan0151 <mehedi2003coc@gmail.com>
2024-05-18 19:02:45 +0600

    added file

---
+++ test.php
@@ -0,0 +1,101 @@
+<?php
+	// class calculation{
+	// 	public $a;
+	// 	function __construct($num){
+	// 		$this->a = $num;	
+	// 	}
+	// 	function su m(){
+	// 		echo $this->a . "<br>";
+	// 	}
+	
+	// }
+	// $a1 = new calculation(1000);
+	
+	// $a1->sum(); 
+
+	
+	
+	
+	
+	// class sale{
+	// 	public $a = "noname"; 	
+	// 	function test(){
+	// 		echo $this->a .  "<br>" . "<br>";
+	// 	}
+	// }
+
+	// $new = new sale();
+	// $new->a = 20;
+	// $new->test();
+
+
+
+
+
+
+    // abstract class vehicles{
+    //     abstract protected function baseSpeed();
+    //     abstract protected function kilo();
+
+    //     function total(int $ajaira){
+    //         echo $totalNeedMoney = $this->baseSpeed() + ($this->kilo() * $ajaira);
+    //         return $totalNeedMoney;
+    //     }   
+    // }
+
+    // class car extends vehicles{
+    //     function baseSpeed(): int{
+    //         return 50;
+    //     }
+    //     function kilo(): int{
+    //         return 2;
+    //     }
+    // }
+
+    // class honda extends vehicles{
+    //     function baseSpeed(): int{
+    //         return 40;
+    //     }
+    //     function kilo(): int{
+    //         return 2;
+    //     }
+    // }
+
+    // $test = new honda;
+    // $test->total(20);
+    
+
+
+    for ($i=0; $i < 10; $i++) { 
+        echo "hello " . $i . "<br>";
+    }
+
+
+
+
+    // abstract class a{
+    //     abstract protected function sum();
+    //     function total(){
+    //         echo " the number is " . $this->sum() . " what";
+    //     }
+    // }
+    
+    
+    // class b extends a{
+    //     function sum(){
+    //         return 500 . " bye";
+    //     }
+    // }
+    // $test = new b;
+    // $test->total();
+
+
+
+
+    // function __autoload($class){
+    //     require $class . ".php";
+    // }
+    
+    
+
+?>
