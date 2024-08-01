825e6198a6ad4d2759ba6dd52b719cf84a6d8474
Mehedihasan0151 <mehedi2003coc@gmail.com>
2024-08-01 10:58:34 +0600

    change some text

--- work.php
+++ work.php
@@ -1,15 +1,11 @@
-<?php
 
-//upload code here
-// then type this 3 line -> jokhon tumi amar kase kisu diba tokhon aita type korba
-// git add . 
-// git commit -m "your massege"
-// git push origin main
-
-
-// ar jokhon ami kisu change korci oita tumi tumar pc te nite chao tokhon aita type korba
-// git pull origin main
-// ami tokhon vul ta disilam thik kore nao
+upload code here
+then type this 3 line -> jokhon tumi amar kase kisu diba tokhon aita type korba -->
+        git add . 
+        git commit -m "your massege"
+        git push origin main
 
 
-?>
+ar jokhon ami kisu change korci oita tumi tumar pc te nite chao tokhon aita type korba -->
+        git pull origin main
+ami tokhon vul ta disilam thik kore nao
