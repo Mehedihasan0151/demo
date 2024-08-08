fbebf6f3c7fb5634f4dbf93a9f861d17316b6615
Mehedihasan0151 <mehedi2003coc@gmail.com>
2024-08-08 12:18:21 +0600

    your massege

--- work.php
+++ work.php
@@ -1,11 +1,17 @@
 
 upload code here
 then type this 3 line -> jokhon tumi amar kase kisu diba tokhon aita type korba -->
+        cd sabiqun
         git add . 
         git commit -m "your massege"
         git push origin main
 
 
 ar jokhon ami kisu change korci oita tumi tumar pc te nite chao tokhon aita type korba -->
+        cd mehedi
         git pull origin main
 ami tokhon vul ta disilam thik kore nao
+
+
+tumi amar kase kisu dile tumar namer jai folder ase oitay lakba ar amar kisu dakte chaile amar namer folder
+a jaiya dakba. amar namer file tay tumi change korleo save hoibo na oita. 
